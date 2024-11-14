54ae9f6533b9cb28cc46fd680dfc7a5c684c4401
Zubek <mateuszsubiel@wp.pl>
2024-11-14 13:43:35 +0100

    finished the challenge

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const mail = document.querySelector('#email');
-const form = document.querySelector('#signupForm');
+const form = document.querySelector('#formComponent');
 const errorMsg = document.querySelector('#error-message');
+const successMsg = document.querySelector('#successMessage');
+const dismissBtn = document.querySelector('#dismissBtn');
+const formWrapper = document.querySelector('#signupForm');
+const successMail = document.querySelector('#successMail');
 
 const emailRegExp =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -18,10 +22,17 @@ const emailRegExp =
         //remove error and proceed with the success message
         errorMsg.classList.add('invisible');
         mail.classList.remove('input-error');
+        formWrapper.classList.add('invisible');
+        successMsg.classList.remove('invisible');
+        successMail.textContent = mail.value;
       }
       else {
         errorMsg.classList.remove('invisible');
         mail.classList.add('input-error');
       }
     }
-    });
+  });
+  dismissBtn.addEventListener('click', () => {
+    formWrapper.classList.remove('invisible');
+    successMsg.classList.add('invisible');
+  })
